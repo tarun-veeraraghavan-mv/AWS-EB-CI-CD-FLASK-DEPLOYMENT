9913dd0bf18badfb27319595d19e0310cb8833cc
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-07-15 19:26:45 -0700

    pls work4

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +14,12 @@ def hello():
 @app.route("/new-route")
 def aws_works():
     return "If this works - AWS CI/CD works!"
+
+secret = os.getenv('AWS_SECRET')
+
+@app.route("/secret")
+def check_aws_secret_works():
+    return f"Secret works or not: {secret}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
